3ae49be68aca3803d48c297e6ac828cf392db6c8
DDIG\csnyder <csnyder@singlestoneconsulting.com>
2016-12-06 21:14:14 -0800

    Initial version of ControlPanel workflow sample

---
+++ Sample Project Folders/Crm/Src/Workflows/ControlPanelActivity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Activities;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+
+namespace SingleStone.Crm.Util
+{
+    public class ControlPanelActivity : CodeActivity
+    {
+        /// <summary>
+        /// Creates a task with a subject equal to the ID of the input EntityReference
+        /// </summary>
+        protected override void Execute(CodeActivityContext executionContext)
+        {
+            IWorkflowContext context = executionContext.GetExtension<IWorkflowContext>();
+            IOrganizationServiceFactory serviceFactory =
+                executionContext.GetExtension<IOrganizationServiceFactory>();
+            IOrganizationService service =
+                serviceFactory.CreateOrganizationService(context.UserId);
+
+            using (var svcContext = new CrmServiceContext(service))
+            {
+                ssc_controlpanel cp = svcContext.ssc_controlpanelSet.FirstOrDefault();
+                if (cp == null)
+                {
+                    throw new InvalidPluginExecutionException("No Control Panel record found.  Please initialize the CSC Control Panel");
+                }
+                controlPanel.Set(executionContext, new EntityReference(ssc_controlpanel.EntityLogicalName, cp.Id));
+
+            }
+        }
+
+        // Define Input/Output Arguments
+
+        [Output("ControlPanel")]
+        [ReferenceTarget("ssc_controlpanel")]
+        public OutArgument<EntityReference> controlPanel { get; set; }
+    }
+}
---
+++ Sample Project Folders/Crm/Src/Workflows/FormatDate.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Activities;
+
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+
+namespace SingleStone.Crm.Util
+{
+    public class FormatDate : CodeActivity
+    {
+        protected override void Execute(CodeActivityContext executionContext)
+        {
+            //Create the tracing service
+            ITracingService tracingService = executionContext.GetExtension<ITracingService>();
+
+            tracingService.Trace("Start formatting");
+            string format = DateFormat.Get(executionContext);
+            DateTime inputDate = DateToFormat.Get(executionContext);
+            if (format != null && inputDate != null)
+            {
+                try
+                {
+                    tracingService.Trace("Format date");
+                    var outputDate = inputDate.ToString(format);
+                    FormattedDate.Set(executionContext, outputDate);
+                }
+                catch (Exception e)
+                {
+                    tracingService.Trace("Error formatting date: " + e.Message);
+                }
+            }
+        }
+
+        [RequiredArgument]
+        [Input("Date To Format")]
+        public InArgument<DateTime> DateToFormat { get; set; }
+
+        [RequiredArgument]
+        [Input("Date Format")]
+        public InArgument<string> DateFormat { get; set; }
+
+        [Output("Formatted Date")]
+        public OutArgument<string> FormattedDate { get; set; }
+    }
+}
